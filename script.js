d63ed4264c5d989afbf39cd4918f5f6d73f2455e
Reda-Yh <redayahyapro@gmail.com>
2024-12-02 00:49:33 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Fonctionnalité du bouton "Rejoindre le serveur"
+document.getElementById('join-button').addEventListener('click', function() {
+    window.open('https://discord.gg/qPTNBxfj', '_blank');
+});
+
+// Fonctionnalité pour ouvrir un lien Discord dans le footer
+document.getElementById('discord-link').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.open('https://discord.gg/qPTNBxfj', '_blank');
+});
