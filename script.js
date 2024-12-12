5fbb0e1f3660247e6ca02fb23536a2fab4151c8a
Reda-Yh <122675645+Reda-Yh@users.noreply.github.com>
2024-12-12 03:31:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Fonctionnalité du bouton "Rejoindre le serveur"
 document.getElementById('join-button').addEventListener('click', function() {
-    window.open('https://discord.gg/qPTNBxfj', '_blank');
+    window.open('https://discord.gg/tckCRRqUaa', '_blank');
 });
 
 // Fonctionnalité pour ouvrir un lien Discord dans le footer
 document.getElementById('discord-link').addEventListener('click', function(event) {
     event.preventDefault();
-    window.open('https://discord.gg/qPTNBxfj', '_blank');
+    window.open('https://discord.gg/tckCRRqUaa', '_blank');
 });
